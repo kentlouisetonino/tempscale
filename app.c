9c6928f1ee2cc682b46fc8891a5e278c5e0e40f0
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-04-27 20:46:38 +0800

    initial commit

---
+++ app.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+  // * application description
+  printf(" ----------------------------------------------------------   \n");
+  printf("|                         TEMTER                           |  \n");
+  printf("|                                                          |  \n");
+  printf("| A simple CLI temperature converter application that will |  \n");
+  printf("| allow you to convert different unit of temperature.      |  \n");
+  printf("|__________________________________________________________|  \n");
+  
+  // * instructions
+  printf("\n");
+  printf("Choose an option:           \n");
+  printf("[1] Celsius to Fahrenheit   \n");
+  printf("[2] Celsius to Kelvin       \n");
+  printf("[3] Fahrenheit to Celsius   \n");
+  printf("[4] Fahrenheit to Kelvin    \n");
+  printf("[5] Kelvin to Celsius       \n");
+  printf("[6] Kelvin to Fahrenheit    \n");
+  
+  // * choice
+  printf("\n");
+  printf("Your choice (1 | 2 | 3 | 4 | 5 | 6): ");
+  printf("\n");
+
+  return 0;
+}
+
