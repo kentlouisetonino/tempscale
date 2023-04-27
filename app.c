be554aaa49fd70251677b51043aff8f4be0f9578
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-04-27 21:21:37 +0800

    add color

--- app.c
+++ app.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#define GREEN "\033[32m"  // * green
+#define RESET "\033[0m"   // * default
 
 int main() {
+  int choice;
+
   // * application description
+  printf(GREEN);
   printf(" ----------------------------------------------------------   \n");
   printf("|                         TEMTER                           |  \n");
   printf("|                                                          |  \n");
   printf("| A simple CLI temperature converter application that will |  \n");
   printf("| allow you to convert different unit of temperature.      |  \n");
   printf("|__________________________________________________________|  \n");
-  
+  printf(GREEN RESET);
+
   // * instructions
   printf("\n");
   printf("Choose an option:           \n");
@@ -22,6 +28,7 @@ int main() {
   // * choice
   printf("\n");
   printf("Your choice (1 | 2 | 3 | 4 | 5 | 6): ");
+  scanf("%d", &choice);
   printf("\n");
 
   return 0;
