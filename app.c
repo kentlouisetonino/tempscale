0792f0edb3918a23b56571579474e0c8b5bbbb79
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-04-27 22:19:13 +0800

    reusable functions

--- app.c
+++ app.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 #define GREEN "\033[32m"  // * green
-#define CYAN "\033[36m"    // * cyan
+#define CYAN "\033[36m"   // * cyan
+#define RED "\033[31m"    // * RED
 #define RESET "\033[0m"   // * default
+
+// * function prototype
+// * defines the function name, return types, and parameters
+void instruction(void);
+int userChoice();
 
 int main() {
   int choice;
@@ -17,6 +25,17 @@ int main() {
   printf(GREEN RESET);
 
   // * instructions
+  instruction();
+  
+  // * choice
+  choice = userChoice();
+
+  printf("\ntest: %d", choice);
+  
+  return 0;
+}
+
+void instruction() {
   printf("\n");
   printf("Choose an option:           \n");
   printf("[1] Celsius to Fahrenheit   \n");
@@ -25,15 +44,18 @@ int main() {
   printf("[4] Fahrenheit to Kelvin    \n");
   printf("[5] Kelvin to Celsius       \n");
   printf("[6] Kelvin to Fahrenheit    \n");
-  
-  // * choice
+}
+
+int userChoice() {
+  int choice;
+
   printf("\n");
   printf(CYAN);
   printf("Your choice (1 | 2 | 3 | 4 | 5 | 6): ");
   printf(CYAN RESET);
   scanf("%d", &choice);
   printf("\n");
-  
-  return 0;
+
+  return choice;
 }
 
