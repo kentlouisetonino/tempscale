d4d26e48c16b55672b67efad0b18794e59c0aa77
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-05-02 00:06:41 +0800

    refactor app

--- app.cpp
+++ app.cpp
@@ -5,6 +5,7 @@
 #include "headers/choicesDescription.h"
 #include "headers/conversionTitle.h"
 #include "headers/conversionCalculation.h"
+#include "headers/optionSelectionPrompt.h"
 #define GREEN "\033[32m"
 #define CYAN "\033[36m"
 #define RED "\033[31m"
@@ -20,44 +21,7 @@ int main() {
   float output;
   bool validOption = true;
 
-  do {
-    applicationDescription();
-    choicesDescription();
-    cout << endl;
-    
-    if (!validOption) {
-      cout << RED;
-      cout << "Input is not valid. Please try again." << endl;
-      cout << endl;
-      cout << RED RESET;
-    }
-  
-    // * inputs
-    cout << "Option (1 | 2 | 3 | 4 | 5 | 6): ";
-    cin >> choice;
-
-    if (cin.fail()) {
-      validOption = false;
-    } else {
-      cout << choice;
-
-      if (
-        choice == 1 ||
-        choice == 2 ||
-        choice == 3 ||
-        choice == 4 ||
-        choice == 5 ||
-        choice  == 6
-      ) {
-        validOption = true;
-      } else {
-        validOption = false;
-      }
-    }
-
-    clearScreen();
-    cleanInputBuffer();
-  } while (!validOption);
+  choice = optionSelectionPrompt();
 
   do {
     cout << endl;
