8963b14e69e1102326dfbfa74262aab4dfc0492e
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-05-01 23:00:48 +0800

    refactor conversion title

--- app.cpp
+++ app.cpp
@@ -3,6 +3,7 @@
 #include "headers/cleanInputBuffer.h"
 #include "headers/applicationDescription.h"
 #include "headers/choicesDescription.h"
+#include "headers/conversionTitle.h"
 #define GREEN "\033[32m"
 #define CYAN "\033[36m"
 #define RED "\033[31m"
@@ -10,7 +11,6 @@
 #define RESET "\033[0m"
 using namespace std;
 
-void conversionTitle(int);
 float conversionProcess(int, float);
 
 int main() {
@@ -90,62 +90,6 @@ int main() {
   } while (!validOption);
 
   return 0;
-}
-
-void conversionTitle(int choice) {
-  if (choice == 1) {
-    cout << CYAN;
-    cout << " -------------------------------------------------------- " << endl;
-    cout << "|                                                        |" << endl;
-    cout << "|             Celsius (C) -> Fahrenheit (F)              |" << endl;
-    cout << "|________________________________________________________|" << endl;
-    cout << CYAN RESET;
-  }
-
-  if (choice == 2) {
-    cout << CYAN;
-    cout << " -------------------------------------------------------- " << endl;
-    cout << "|                                                        |" << endl;
-    cout << "|             Celsius (C) -> Kelvin (K)                  |" << endl;
-    cout << "|________________________________________________________|" << endl;
-    cout << CYAN RESET;
-  }
-
-  if (choice == 3) {
-    cout << CYAN;
-    cout << " -------------------------------------------------------- " << endl;
-    cout << "|                                                        |" << endl;
-    cout << "|             Fahrenheit (F) -> Celsius (C)              |" << endl;
-    cout << "|________________________________________________________|" << endl;
-    cout << CYAN RESET;
-  } 
-
-  if (choice == 4) {
-    cout << CYAN;
-    cout << " -------------------------------------------------------- " << endl;
-    cout << "|                                                        |" << endl;
-    cout << "|             Fahrenheit (F) -> Kelvin (K)               |" << endl;
-    cout << "|________________________________________________________|" << endl;
-    cout << CYAN RESET;
-  }
-
-  if (choice == 5) {
-    cout << CYAN;
-    cout << " -------------------------------------------------------- " << endl;
-    cout << "|                                                        |" << endl;
-    cout << "|             Kelvin (K) -> Celsius (C)                  |" << endl;
-    cout << "|________________________________________________________|" << endl;
-    cout << CYAN RESET;
-  }
-
-  if (choice == 6) {
-    cout << CYAN;
-    cout << " -------------------------------------------------------- " << endl;
-    cout << "|                                                        |" << endl;
-    cout << "|             Kelvin (F) -> Fahrenheit (F)               |" << endl;
-    cout << "|________________________________________________________|" << endl;
-    cout << CYAN RESET;
-  }
 }
 
 float conversionProcess(int choice, float conversionInput) {
