223fcb6c041c594859b079c9472949f8d6e19a81
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-26 20:55:35 +0800

    Update the application description.

--- src/displays/app_description.go
+++ src/displays/app_description.go
@@ -3,11 +3,11 @@ package displays
 import "fmt"
 
 func AppDescription() {
-	fmt.Println("\t", ColorBlue, "---------------------------------------------------------", ColorReset)
+	fmt.Println(Tab, ColorBlue, "---------------------------------------------------------", ColorReset)
 	AddNewline()
-	fmt.Println("\t\t\t\t", ColorGreen, "TempScale", ColorReset)
+	fmt.Println(Tab, Tab, Tab, Tab, ColorGreen, "TempScale", ColorReset)
 	AddNewline()
-	fmt.Println("\t\t", "A CLI tool for converting temperature scale.")
+	fmt.Println(Tab, Tab, "A CLI tool for converting temperature scale.")
 	AddNewline()
-	fmt.Println("\t", ColorBlue, "---------------------------------------------------------", ColorReset)
+	fmt.Println(Tab, ColorBlue, "---------------------------------------------------------", ColorReset)
 }
