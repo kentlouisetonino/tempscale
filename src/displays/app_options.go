1099c7c38a8ec0c3819569c170142fcb965abacf
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-27 14:33:12 +0800

    Include the options.

--- src/displays/app_options.go
+++ src/displays/app_options.go
@@ -1,10 +1,20 @@
 package displays
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+const openBracket = ColorBlue + "[" + ColorReset
+const closeBracket = ColorBlue + "]" + ColorReset
+
+func optionNumber(number int) string {
+	return ColorGreen + strconv.Itoa(number) + ColorReset
+}
 
 func AppOptions() {
-	fmt.Println(Tab, " ", "[1]", "Celsius (°C)")
-	fmt.Println(Tab, " ", "[2]", "Fahrenheit (°F)")
-	fmt.Println(Tab, " ", "[3]", "Kelvin (°K)")
-	fmt.Println(Tab, " ", "[4]", "Rankine (°R)")
+	fmt.Println(Tab, " ", openBracket, optionNumber(1), closeBracket, "Celsius (°C)")
+	fmt.Println(Tab, " ", openBracket, optionNumber(2), closeBracket, "Fahrenheit (°F)")
+	fmt.Println(Tab, " ", openBracket, optionNumber(3), closeBracket, "Kelvin (°K)")
+	fmt.Println(Tab, " ", openBracket, optionNumber(4), closeBracket, "Rankine (°R)")
 }
