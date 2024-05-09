1ec7b0bfeba31db31f98e14078493a0779e8eb3a
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-05-09 21:41:34 +0800

    Handle input for celsius.

--- src/services/celsius.go
+++ src/services/celsius.go
@@ -1,15 +1,38 @@
 package services
 
-import "github.com/kentlouisetonino/tempscale/src/displays"
+import (
+	"fmt"
+
+	"github.com/kentlouisetonino/tempscale/src/displays"
+)
 
 func Celsius() {
-	// Clear the screen.
-	displays.ClearScreen()
-	displays.AddNewline()
-	displays.AddNewline()
+	var celsiusInput float64
+	var celsiusInputError bool = false
 
-	// Show the description.
-	displays.CelsiusOptionDescription()
-	displays.AddNewline()
-	displays.AddNewline()
+	for {
+		// Clear the screen.
+		displays.ClearScreen()
+		displays.AddNewline()
+		displays.AddNewline()
+
+		// Show the description.
+		displays.CelsiusOptionDescription()
+		displays.AddNewline()
+
+		// Error instruction.
+		if celsiusInputError {
+			displays.AppOptionInputError()
+			displays.AddNewline()
+		}
+
+		// Ask the celsius input.
+		fmt.Print(displays.Tab, "   Celsius Input (°C): ")
+		_, err := fmt.Scan(&celsiusInput)
+
+		if err != nil {
+			celsiusInputError = true
+			continue
+		}
+	}
 }
